a7a6ab70fddd130a1d0fac06146547937f4e0805
Chiaen Fu <peterfuwork@gmail.com>
2020-01-13 14:38:37 -0700

    vue video app

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="container">
+		<SearchBar @termChange="onTermChange"></SearchBar>
+		<div class="row">
+			<VideoDetail :video="selectedVideo" />
+			<VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY = 'AIzaSyD8NShqwo82FeRngn26l6xsGjtKkPdKFd0';
+
+export default {
+	name: 'App',
+	components: {
+		SearchBar,
+		VideoList,
+		VideoDetail
+	},
+	data: function() {
+		return {
+			videos: [],
+			selectedVideo: null
+		};
+	},
+	methods: {
+		onVideoSelect(video) {
+			this.selectedVideo = video;
+		},
+		onTermChange(searchTerm) {
+			axios
+				.get('https://www.googleapis.com/youtube/v3/search', {
+					params: {
+						key: API_KEY,
+						type: 'video',
+						part: 'snippet',
+						q: searchTerm
+					}
+				})
+				.then((response) => {
+					this.videos = response.data.items;
+					this.selectedVideo = response.data.items[0];
+				})
+				.catch((error) => {
+					window.console.log(error);
+				});
+		}
+	},
+	created() {
+		this.onTermChange('copperband fish');
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+	width: 75%;
+}
+</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,35 @@
+<template>
+	<li class="list-group-item media" @click="onVideoSelect">
+		<img class="mr-3" :src="thumbnailUrl" :alt="video.snippet.title" />
+		<div class="media-body">
+			{{ video.snippet.title }}
+		</div>
+	</li>
+</template>
+
+<script>
+export default {
+	name: 'VideoListItem',
+	props: ['video'],
+	computed: {
+		thumbnailUrl() {
+			return this.video.snippet.thumbnails.default.url;
+		}
+	},
+	methods: {
+		onVideoSelect() {
+			this.$emit('videoSelect', this.video);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+li.list-group-item {
+	display: flex;
+	cursor: pointer;
+	&:hover {
+		background-color: #eee;
+	}
+}
+</style>
